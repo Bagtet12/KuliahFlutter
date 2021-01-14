596fe5614447ef4f2042eea0e8d5da88c45fdfb6
Bagtet12 <72537003+Bagtet12@users.noreply.github.com>
2021-01-14 21:12:19 +0700

    Update title_dan_button.dart

--- lib/screens/components/title_dan_button.dart
+++ lib/screens/components/title_dan_button.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 
+<<<<<<< HEAD
+=======
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 92c097c24a76eaa196736a96aa854bb88d218065
 import '../../constant.dart';
 
 class TitleWithLainnyaBtn extends StatelessWidget {
   const TitleWithLainnyaBtn({
+<<<<<<< HEAD
+=======
+=======
+=======
+>>>>>>> 9c4b41d2bf5eba9e6eae1faea1750340e89f7a68
+>>>>>>> Stashed changes
+import '../../../constants.dart';
+
+class TitleWithBtn extends StatelessWidget {
+  const TitleWithBtn({
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+>>>>>>> 9c4b41d2bf5eba9e6eae1faea1750340e89f7a68
+=======
+>>>>>>> 9c4b41d2bf5eba9e6eae1faea1750340e89f7a68
+>>>>>>> Stashed changes
+>>>>>>> 92c097c24a76eaa196736a96aa854bb88d218065
     Key key,
     this.title,
     this.press,
@@ -18,7 +43,23 @@ class TitleWithLainnyaBtn extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
       child: Row(
         children: [
+<<<<<<< HEAD
           TitleWithUnderline(text: title),
+=======
+<<<<<<< Updated upstream
+          TitleWithCustomUnderline(text: title),
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+          TitleWithUnderline(text: title),
+=======
+          TitleWithCustomUnderline(text: title),
+>>>>>>> 9c4b41d2bf5eba9e6eae1faea1750340e89f7a68
+=======
+          TitleWithCustomUnderline(text: title),
+>>>>>>> 9c4b41d2bf5eba9e6eae1faea1750340e89f7a68
+>>>>>>> Stashed changes
+>>>>>>> 92c097c24a76eaa196736a96aa854bb88d218065
           Spacer(),
           FlatButton(
             shape: RoundedRectangleBorder(
@@ -26,7 +67,23 @@ class TitleWithLainnyaBtn extends StatelessWidget {
             ),
             color: kPrimaryColor,
             onPressed: press,
+<<<<<<< HEAD
             child: Text("Lainnya", style: TextStyle(color: Colors.white),
+=======
+<<<<<<< Updated upstream
+            child: Text("Lainnya",style: TextStyle(color: Colors.white),
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+            child: Text("Lainnya", style: TextStyle(color: Colors.white),
+=======
+            child: Text("Lainnya",style: TextStyle(color: Colors.white),
+>>>>>>> 9c4b41d2bf5eba9e6eae1faea1750340e89f7a68
+=======
+            child: Text("Lainnya",style: TextStyle(color: Colors.white),
+>>>>>>> 9c4b41d2bf5eba9e6eae1faea1750340e89f7a68
+>>>>>>> Stashed changes
+>>>>>>> 92c097c24a76eaa196736a96aa854bb88d218065
             ),
           ),
         ],
@@ -48,7 +105,23 @@ class TitleWithUnderline extends StatelessWidget {
     return Container(
       height: 24,
       child: Stack(
+<<<<<<< HEAD
         children: <Widget> [
+=======
+<<<<<<< Updated upstream
+        children:[
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+        children: <Widget> [
+=======
+        children:[
+>>>>>>> 9c4b41d2bf5eba9e6eae1faea1750340e89f7a68
+=======
+        children:[
+>>>>>>> 9c4b41d2bf5eba9e6eae1faea1750340e89f7a68
+>>>>>>> Stashed changes
+>>>>>>> 92c097c24a76eaa196736a96aa854bb88d218065
           Padding(
             padding: const EdgeInsets.only(left: kDefaultPadding / 4),
             child: Text(
@@ -65,7 +138,23 @@ class TitleWithUnderline extends StatelessWidget {
               height: 7,
               color: kPrimaryColor.withOpacity(0.2),
             ),
+<<<<<<< HEAD
           ),
+=======
+<<<<<<< Updated upstream
+          )
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+          ),
+=======
+          )
+>>>>>>> 9c4b41d2bf5eba9e6eae1faea1750340e89f7a68
+=======
+          )
+>>>>>>> 9c4b41d2bf5eba9e6eae1faea1750340e89f7a68
+>>>>>>> Stashed changes
+>>>>>>> 92c097c24a76eaa196736a96aa854bb88d218065
         ],
       ),
     );
