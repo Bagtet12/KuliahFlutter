e4c249c7a02898e4980304a0a5ff117461916b07
Bagtet12 <72537003+Bagtet12@users.noreply.github.com>
2021-01-14 21:12:45 +0700

    Update home_screen.dart

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,9 +3,12 @@ import 'package:save_earth/constant.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:save_earth/screens/components/body.dart';
 import 'package:save_earth/screens/components/bottom_menu_nav.dart';
+<<<<<<< HEAD
 import 'package:http/http.dart' as Http;
 import 'dart:async';
 import 'dart:convert';
+=======
+>>>>>>> 92c097c24a76eaa196736a96aa854bb88d218065
 
 
 class HomeScreen extends StatelessWidget{
